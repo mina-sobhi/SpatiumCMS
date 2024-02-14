6a575b168425f12474176abdc2ba90d6808fc9f9
mohammedatefa <mohamed.atef.abdelsbor@gmail.com>
2024-02-14 17:11:19 +0200

    Add Validation to Post Request

--- Spatium-CMS/Controllers/PostController/Request/CreatePostRequest.cs
+++ Spatium-CMS/Controllers/PostController/Request/CreatePostRequest.cs
@@ -1,15 +1,34 @@
-﻿namespace Spatium_CMS.Controllers.PostController.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spatium_CMS.Controllers.PostController.Request
 {
     public class CreatePostRequest
     {
+        [Required(ErrorMessage ="Title is Required")]
+        [MinLength(3,ErrorMessage ="the length must mbe max 3 charachters")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Slug is Required")]
+        [MinLength(3, ErrorMessage = "the length must mbe max 3 charachters")]
         public string Slug { get; set; }
+
+        [Required(ErrorMessage = "FeaturedImagePath is Required")]
+        [MinLength(3, ErrorMessage = "the length must mbe max 3 charachters")]
         public string FeaturedImagePath { get; set; }
+
+        [Required(ErrorMessage = "Content is Required")]
+        [MinLength(3, ErrorMessage = "the length must mbe max 3 charachters")]
         public string Content { get; set; }
+        [Required(ErrorMessage = "MetaDescription is Required")]
+        [MinLength(3, ErrorMessage = "the length must mbe max 3 charachters")]
         public string MetaDescription { get; set; }
         public int ContentLineSpacing { get; set; }
+        [Required(ErrorMessage = "Category is Required")]
+        [MinLength(3, ErrorMessage = "the length must mbe max 3 charachters")]
         public string Category { get; set; }
+        [Required(ErrorMessage = "Tag is Required")]
+        [MinLength(3, ErrorMessage = "the length must mbe max 3 charachters")]
         public string Tag { get; set; }
+
         //public string PublishDate { get; set; }
         //public string UnPublishDate { get; set; }
         public string AuthorId { get; set; }
