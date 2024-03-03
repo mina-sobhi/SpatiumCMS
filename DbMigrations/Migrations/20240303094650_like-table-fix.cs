0c37f72c852a1932f611ea84b42b70e08bc63568
Mina Awad <mina_awad2030@yahoo.com>
2024-03-03 11:47:21 +0200

    LikeTable Fix

---
+++ DbMigrations/Migrations/20240303094650_like-table-fix.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class liketablefix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Like");
+
+            migrationBuilder.DropColumn(
+                name: "IsLike",
+                table: "Like");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Like",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLike",
+                table: "Like",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
