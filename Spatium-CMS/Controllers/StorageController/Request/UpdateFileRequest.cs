9155104c30fdba5320444d53efed7827043b2984
Manar <manarmaher835@gmail.com>
2024-03-06 15:14:21 +0200

    updatefile and create handling

--- Spatium-CMS/Controllers/StorageController/Request/UpdateFileRequest.cs
+++ Spatium-CMS/Controllers/StorageController/Request/UpdateFileRequest.cs
@@ -4,11 +4,11 @@
     {
 
         public int Id { get; set; }
-     
+        
         public string Name { get; set; }
         public string Caption { get; set; }
         public string Alt { get; set; }
         public string Dimension { get; set; }
-        public IFormFile File { get; set; }
+        public IFormFile? File { get; set; }
     }
 }
