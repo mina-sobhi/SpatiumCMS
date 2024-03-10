75d993cace2536b977f180270827555370c51859
hesham-1999 <154536107+hesham-1999@users.noreply.github.com>
2024-03-10 16:19:11 +0200

    Add Validation in UserManagement

--- Spatium-CMS/Controllers/UserManagmentController/Request/UpdateUserPaswordRequest.cs
+++ Spatium-CMS/Controllers/UserManagmentController/Request/UpdateUserPaswordRequest.cs
@@ -6,9 +6,11 @@ namespace Spatium_CMS.Controllers.UserManagmentController.Request
     {
     
         [Required(ErrorMessage = " Currenet Password is Required ")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",ErrorMessage = "-At least 8 characters long\r\n- Contains at least one uppercase letter (A-Z)\r\n- Contains at least one lowercase letter (a-z)\r\n- Contains at least one digit (0-9)\r\n- Contains at least one special character")]
         public string CurrenetPassword { get; set; }
 
         [Required(ErrorMessage = " New Password is Required ")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "-At least 8 characters long\r\n- Contains at least one uppercase letter (A-Z)\r\n- Contains at least one lowercase letter (a-z)\r\n- Contains at least one digit (0-9)\r\n- Contains at least one special character")]
         public string NewPassword { get; set; }
 
         [Required(ErrorMessage = "Confirm New Password is Required ")]
